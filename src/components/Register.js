2255c50a7eae962d7f5a77698b2b0c400dac4fe6
rhidayscrew17 <rhidayscrew@gmail.com>
2022-08-03 20:36:07 +0700

    update revisi test netlify

--- src/components/Register.js
+++ src/components/Register.js
@@ -19,7 +19,7 @@ const Register = () => {
                 password: password,
                 confPassword: confPassword
             });
-            navigate("/register");
+            navigate("/");
         } catch (error) {
             if (error.response) {
                 setMsg(error.response.data.msg);
