b428ae58a90bd60953f977fec0b0e33e147118f3
rhidayscrew17 <rhidayscrew@gmail.com>
2022-08-03 15:57:25 +0700

    update hapus credential2

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -18,7 +18,7 @@ const Dashboard = () => {
 
     const refreshToken = async () => {
         try {
-            const response = await axios.get('http://updatenutechnodemysql.herokuapp.com/token', { credentials: 'include' });
+            const response = await axios.get('http://updatenutechnodemysql.herokuapp.com/token');
             setToken(response.data.accessToken);
             const decoded = jwt_decode(response.data.accessToken);
             setName(decoded.name);
@@ -35,7 +35,7 @@ const Dashboard = () => {
     axiosJWT.interceptors.request.use(async (config) => {
         const currentDate = new Date();
         if (expire * 1000 < currentDate.getTime()) {
-            const response = await axios.get('http://updatenutechnodemysql.herokuapp.com/token', { credentials: 'include' });
+            const response = await axios.get('http://updatenutechnodemysql.herokuapp.com/token');
             config.headers.Authorization = `Bearer ${response.data.accessToken}`;
             setToken(response.data.accessToken);
             const decoded = jwt_decode(response.data.accessToken);
